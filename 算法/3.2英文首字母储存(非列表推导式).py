4c8d4431337036f788c958e45cdefc8e86de3f38
小卿 <118453950+lubiandewoheni@users.noreply.github.com>
2024-09-19 11:22:02 +0800

    Delete 算法 directory

--- 算法/3.2英文首字母储存(非列表推导式).py
+++
@@ -1,8 +0,0 @@
-#使用循环取出"My house is full of toys"中每个单词的首字母，并存储到列表对象中
-
-s = "My house is full of toys"
-words = s.split()
-initials = []
-for word in words:
-    initials.append(word[0])
-print(initials)
--- 算法/G. 带’ 和 ”字符的输出.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	char a;
-	scanf("%c",&a);
-	printf("'%c'\n",a);
-	printf("\"%c\"",a);
-}
